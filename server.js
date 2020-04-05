dd670d63391248aea68a1c61714583658502de05
Drozerah <drozerah@gmail.com>
2020-04-05 16:24:06 +0200

    feat: add sync bcript hash

--- server.js
+++ server.js
@@ -13,16 +13,18 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash){
+  console.log(hash)
+  bcrypt.compare(myPlaintextPassword, hash, function(err, res){
+    console.log(res)
+  })
+})
 //END_ASYNC
 
 //START_SYNC
-
-
-
 //END_SYNC
 
 
-app.listen(process.env.PORT || 3000, () => {});
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening ${process.env.PORT || 3000}`) // !DEBUG
+});
